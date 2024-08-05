2bff8d1688a79e96ef1b989b693aedec0ec1b074
rvijayar <rvijayar@sfu.ca>
2024-08-04 17:17:10 -0700

    add temp UUID constants

---
+++ app/src/main/java/com/passer/passwatch/core/ble/UUIDConstants.kt
@@ -0,0 +1,18 @@
+package com.passer.passwatch.core.ble
+
+/* Type 5 UUIDs
+com.passer.passwatch.main.service : 34a7b4b1-0846-5a85-8f33-70956d967b1d
+com.passer.passwatch.main.characteristic.time : 34a7b4b1-0846-5a85-8f33-70956d967b1d
+com.passer.passwatch.main.characteristic.speed : 27870e2a-eb1a-5568-aecf-2997368d089b
+com.passer.passwatch.main.characteristic.distance : e78052ca-8645-5683-aff5-3bdcd21cceaa
+com.passer.passwatch.main.characteristic.video_id : 1f732663-51e7-576c-a653-932bbab3de96
+com.passer.passwatch.main.characteristic.near_pass_flag : 45f8d0a4-6fc7-56c7-b3cc-9fff3af4673d
+*/
+
+const val serviceUUID = "34a7b4b1-0846-5a85-8f33-70956d967b1d"
+const val timeCharacteristic = "34a7b4b1-0846-5a85-8f33-70956d967b1d"
+const val speedCharacteristic = "27870e2a-eb1a-5568-aecf-2997368d089b"
+const val distanceCharacteristic = "e78052ca-8645-5683-aff5-3bdcd21cceaa"
+const val videoIdCharacteristic = "1f732663-51e7-576c-a653-932bbab3de96"
+const val nearPassFlagCharacteristic = "45f8d0a4-6fc7-56c7-b3cc-9fff3af4673d"
+
